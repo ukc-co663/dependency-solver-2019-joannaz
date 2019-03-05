2b3537fe306767ed0cf2ad64d0ef1e724568686d
Joanna Zhang <jz225@kent.ac.uk>
2019-03-05 20:43:11 +0000

    Add lib

--- install_deps.sh
+++ install_deps.sh
@@ -5,3 +5,4 @@ sudo wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filep
 sudo wget -O lib/logicng-1.4.1.jar http://central.maven.org/maven2/org/logicng/logicng/1.4.1/logicng-1.4.1.jar
 sudo wget -O lib/guava-25.1-jre.jar http://central.maven.org/maven2/com/google/guava/guava/25.1-jre/guava-25.1-jre.jar
 sudo wget -O lib/antlr4-runtime-4.7.jar http://central.maven.org/maven2/org/antlr/antlr4-runtime/4.7/antlr4-runtime-4.7.jar
+sudo wget -O lib/jgrapht-jdk1.5-0.7.3.jar http://central.maven.org/maven2/org/jgrapht/jgrapht-jdk1.5/0.7.3/jgrapht-jdk1.5-0.7.3.jar
