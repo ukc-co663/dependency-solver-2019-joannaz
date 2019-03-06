3bab1ea5805d8313971750dce8f01faf70363f24
Joanna Zhang <jz225@kent.ac.uk>
2019-03-06 00:46:42 +0000

    nani desu ka

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -27,31 +27,39 @@ public class Main {
 	
 
 	public static void main(String[] args) throws IOException {
+		
+		TypeReference<List<Package>> repoType = new TypeReference<List<Package>>() {};
+	    List<Package> repo = JSON.parseObject(readFile(args[0]), repoType);
+	    TypeReference<List<String>> strListType = new TypeReference<List<String>>() {};
+	    List<String> initial = JSON.parseObject(readFile(args[1]), strListType);
+	    List<String> toInstall = JSON.parseObject(readFile(args[2]), strListType);
 
-		String repoPath 		= 	null;
-		String initialPath 		=  	null;
-		String constraintsPath 	= 	null;
+		
 
-		if(args.length == 0) {
-			repoPath = "tests/seen-7/repository.json";
-			initialPath = "tests/seen-7/initial.json";
-			constraintsPath = "tests/seen-7/constraints.json";
-		} else {
-			repoPath 		= args[0];
-			initialPath 	= args[1];
-			constraintsPath = args[2];
-		}
-
-
-		TypeReference<List<String>> strListType = new TypeReference<List<String>>() {};
-		TypeReference<List<Package>> repoType 	= new TypeReference<List<Package>>() {};
-		// Repo
-		repo = JSON.parseObject(readFile(repoPath), repoType);
-		realRepo = new Repository(repo);
-		// Initial state
-		initial = JSON.parseObject(readFile(initialPath), strListType);
-		// What I need to install
-		toInstall = JSON.parseObject(readFile(constraintsPath), strListType);    
+//		String repoPath 		= 	null;
+//		String initialPath 		=  	null;
+//		String constraintsPath 	= 	null;
+//
+//		if(args.length == 0) {
+//			repoPath = "tests/seen-0/repository.json";
+//			initialPath = "tests/seen-0/initial.json";
+//			constraintsPath = "tests/seen-0/constraints.json";
+//		} else {
+//			repoPath 		= args[0];
+//			initialPath 	= args[1];
+//			constraintsPath = args[2];
+//		}
+//
+//
+//		TypeReference<List<String>> strListType = new TypeReference<List<String>>() {};
+//		TypeReference<List<Package>> repoType 	= new TypeReference<List<Package>>() {};
+//		// Repo
+//		repo = JSON.parseObject(readFile(repoPath), repoType);
+//		realRepo = new Repository(repo);
+//		// Initial state
+//		initial = JSON.parseObject(readFile(initialPath), strListType);
+//		// What I need to install
+//		toInstall = JSON.parseObject(readFile(constraintsPath), strListType);    
 
 		realRepo.calculateDeps(repo);
 		realRepo.calculateConflicts(repo);
