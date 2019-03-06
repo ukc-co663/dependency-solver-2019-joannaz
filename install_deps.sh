94e96018dfda502f5d36d50d41504454d61baa09
Joanna Zhang <jz225@kent.ac.uk>
2019-03-06 00:35:21 +0000

    hello??

--- install_deps.sh
+++ install_deps.sh
@@ -5,5 +5,5 @@ wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=c
 wget -O lib/logicng-1.4.1.jar http://central.maven.org/maven2/org/logicng/logicng/1.4.1/logicng-1.4.1.jar
 wget -O lib/guava-25.1-jre.jar http://central.maven.org/maven2/com/google/guava/guava/25.1-jre/guava-25.1-jre.jar
 wget -O lib/antlr4-runtime-4.7.jar http://central.maven.org/maven2/org/antlr/antlr4-runtime/4.7/antlr4-runtime-4.7.jar
-wget -O lib/jgrapht-jdk1.5-0.7.3.jar http://central.maven.org/maven2/org/jgrapht/jgrapht-jdk1.5/0.7.3/jgrapht-jdk1.5-0.7.3.jar
-wget -O lib/jgraph-5.8.3.1.jar http://central.maven.org/maven2/jgraph/jgraph/5.8.3.1/jgraph-5.8.3.1.jar
+wget -O lib/jgrapht-core-1.3.0.jar http://central.maven.org/maven2/org/jgrapht/jgrapht-core/1.3.0/jgrapht-core-1.3.0.jar
+wget -O lib/jheaps-0.9.jar http://central.maven.org/maven2/org/jheaps/jheaps/0.9/jheaps-0.9.jar
